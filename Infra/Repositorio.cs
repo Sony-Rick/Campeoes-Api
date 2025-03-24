322e2e7e31e9853e166970766d46bd2b0d9068c1
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2025-03-24 19:41:17 -0300

    Infra Repositorio (ajustar repositorio inplementado)

---
+++ Infra/Repositorio.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entidade;
+using Domain.Interface;
+
+namespace Infra.Repositorio;
+
+
+public class CampeaoRepositorio : ICampeaoRepositorio
+{
+    private readonly List<Campeao> _campeao = new List<Campeao>();
+    public void Adicionar(Campeao campeao)
+    {
+        _campeao.Add(campeao);
+    }	
+
+    public Campeao GetById(int id)
+    {
+        return _campeao.FirstOrDefault(c =>c.Id == id) ?? new Campeao();        
+    }
+
+    public IEnumerable<Campeao> GetAll()
+    {
+        return _campeao;
+    }
+
+    public void Update(Campeao campeao)
+    {
+        var CampeaoExistente = GetById(campeao.Id);
+        if (CampeaoExistente != null)
+        {
+            CampeaoExistente.Nome = campeao.Nome;
+            CampeaoExistente.lane = campeao.lane;
+            CampeaoExistente.tipo = campeao.tipo;
+        }
+    }
+
+    public void Delete(int id)
+        {
+            var campeao = GetById(id);
+            if (campeao != null)
+            {
+                _campeao.Remove(campeao);
+            }
+        }
+}
--- Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
+++ Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f068e2a9d91810ccc8604df28754e64a384ef8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d7ad270ecab4d2b89327b88b7fa47ba876273b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
