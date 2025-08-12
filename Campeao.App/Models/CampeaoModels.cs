3f29a3f4fe258b8f1f08e416333242e4f80e7b18
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2025-08-12 20:46:27 -0300

    CampeaoModel revisão

--- Campeao.App/Models/CampeaoModels.cs
+++ Campeao.App/Models/CampeaoModels.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Campeao.Domain.Enums;
+using Campeao.Domain.Entities;
+using Campeao.Domain.ObjetosDeValor;
 
 namespace Campeao.App.Models;
 
@@ -11,8 +13,21 @@ public class CampeaoModels
     [Required(ErrorMessage = "O campo {0} é obrigatorio")]
     public Tipo TipoCampeao { get; set;}
     
-    [Required(ErrorMessage = "É necessario no minimo uma lane selecionada")]
+    [Required(ErrorMessage = "É necessario no minimo uma {0} selecionada")]
     public CampeaoPosicao Lane { get; set; }
     
+    [Required(ErrorMessage = "É necessario adicionar a vida base do Campeão")]
+    public int VidaBase  { get; set; }
     
+    [Required(ErrorMessage = "É necessario adicionar a Mana Base do Campeao")]
+    public int ManaBase { get; set; }
+    
+    [Required(ErrorMessage = "É necessario adicionar o Dano de Ataque Basico")]
+    public int DanoAtaqueBasico { get; set; }
+    
+    [Required(ErrorMessage = "É necessario adicionar Passiva do Campeão")]
+    public string Passiva  { get; set; }
+    
+    [Required(ErrorMessage = "Por Favor Adicione Todas as Habilidades do Campeão")]
+    public Habilidades Habilidades { get; set; }
 }
